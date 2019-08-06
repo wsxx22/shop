fe928110ba6c24ba9b474d5d066f9f77323d1d65
Daniel Bitka <daniel.bitka@gmail.com>
2019-08-07 00:42:30 +0200

    created userRoleDto

---
+++ src/main/java/com/makeup/user/domain/dto/UserRoleDto.java
@@ -0,0 +1,16 @@
+package com.makeup.user.domain.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRoleDto {
+
+
+
+    String type;
+}
