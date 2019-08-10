02dd0e1c52a9cb19734eaf53967d34a8aefdb083
Daniel Bitka <daniel.bitka@gmail.com>
2019-08-10 11:54:04 +0200

    update user validator

--- src/main/java/com/makeup/user/domain/UserValidator.java
+++ src/main/java/com/makeup/user/domain/UserValidator.java
@@ -2,6 +2,7 @@ package com.makeup.user.domain;
 
 import com.makeup.user.domain.dto.CreateUserDto;
 import com.makeup.user.domain.exceptions.InvalidUserException;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -14,23 +15,18 @@ import java.util.regex.Pattern;
 import static com.makeup.user.domain.exceptions.InvalidUserException.CAUSE.*;
 
 @Slf4j
-@FieldDefaults(level = AccessLevel.PRIVATE)
-//@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@AllArgsConstructor
 class UserValidator {
     UserRepository userRepository;
-    final int loginLength = 5;
-
-    public UserValidator(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     void checkInputDto(CreateUserDto createUserDto){
         String login = createUserDto.getLogin();
 
         checkValueIfBlank(login, new InvalidUserException(LOGIN_BLANK));
-        checkLengthValue(login, new InvalidUserException(LOGIN_LENGTH));
+        checkLoginLength(login, new InvalidUserException(LOGIN_LENGTH));
         userRepository.findByLogin(login).ifPresent(user -> {
-//            log.error(LOGIN_EXISTS.getMessage());
+            log.error(LOGIN_EXISTS.getMessage());
             throw new InvalidUserException(LOGIN_EXISTS);
         });
 
@@ -39,9 +35,9 @@ class UserValidator {
 
     private void validateEmail(String email){
         checkValueIfBlank(email, new InvalidUserException(LOGIN_BLANK));
-        checkByRegExp(email);
+        checkEmailByRegExp(email);
         userRepository.findByEmail(email).ifPresent(user -> {
-//            log.error(EMAIL_EXISTS.getMessage());
+            log.error(EMAIL_EXISTS.getMessage());
             throw new InvalidUserException(EMAIL_EXISTS);
         });
     }
@@ -51,23 +47,22 @@ class UserValidator {
             throwException(exception);
     }
 
-    private void checkLengthValue(String value, RuntimeException exception){
-        if (value.length() < loginLength)
+    private void checkLoginLength(String value, RuntimeException exception){
+        if (value.length() < 5)
             throwException(exception);
     }
 
-    private void checkByRegExp(String email) {
+    private void checkEmailByRegExp(String email){
         Pattern pattern = Pattern.compile("^[a-zA-Z0-9.]+@[a-zA-Z0-9]+.+[a-zA-Z0-9.]+.{0}$", Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(email);
 
-        if (matcher.find()){
-//            log.error(EMAIL_INCORRECT.getMessage());
+       if (!pattern.matcher(email).matches()){
+            log.error(EMAIL_INCORRECT.getMessage());
             throw new InvalidUserException(EMAIL_INCORRECT);
         }
     }
 
-    private void throwException(RuntimeException exception) {
-//        log.error(exception.getMessage());
+    private void throwException(RuntimeException exception){
+        log.error(exception.getMessage());
         throw exception;
     }
 }
