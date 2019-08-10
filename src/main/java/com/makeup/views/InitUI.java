ce77d60b8d0178da462173606f3b5bd863223f64
Daniel Bitka <daniel.bitka@gmail.com>
2019-08-10 11:56:01 +0200

    add initView, homeView, RegisterAccView

---
+++ src/main/java/com/makeup/views/InitUI.java
@@ -0,0 +1,17 @@
+package com.makeup.views;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.spring.annotation.SpringViewDisplay;
+import com.vaadin.ui.UI;
+
+@SpringUI
+@SpringViewDisplay
+public class InitUI extends UI {
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        getUI().getNavigator().navigateTo("homepage");
+        getNavigator().setErrorView(InitUI.getCurrent().getNavigator().getCurrentView());
+
+    }
+}
